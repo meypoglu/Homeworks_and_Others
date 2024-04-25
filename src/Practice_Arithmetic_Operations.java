0f7b1356587612a5fe71701c655ef5972a4247ef
Mirac Eyüpoğlu <167072172+meypoglu@users.noreply.github.com>
2024-04-25 10:50:38 +0300

    Update Practice_Arithmetic_Operations.java

--- src/Practice_Arithmetic_Operations.java
+++ src/Practice_Arithmetic_Operations.java
@@ -6,11 +6,12 @@ public class Practice_Arithmetic_Operations {
     public static void main(String[] args) {
         // Scanner ile kullanıcıdan girdi alındı
         Scanner input = new Scanner(System.in);
-        System.out.print("Üç adet sayı giriniz: ");
-        // Boşlukla ayrılan sayılar art arda kullanılan nextDouble ile alındı
+        System.out.print("Birinci sayıyı giriniz: ");
         double num1 = input.nextDouble();
+        System.out.print("İkinci sayıyı giriniz: ");
         double num2 = input.nextDouble();
-        double num3 = input.nextDouble();
+        System.out.print("Üçüncü sayıyı giriniz: ");
+        double num3 = input.nextDouble(); 
         // Sonucun hesaplanıp bastırılması
         System.out.println("Sonuç: " + ((num2 * num3) + num1 - num3));
     }
